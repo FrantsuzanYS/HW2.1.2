20303ab8b260f58dc1c0c291e2bee546e0039508
Frantsuzan_YS <niajulya@icloud.com>
2021-07-07 19:12:41 +0300

    final work

--- HW2.1.2/ViewController.swift
+++ HW2.1.2/ViewController.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 
+var beginButton = 0
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLampView: UIView!
+    @IBOutlet weak var yellowLampView: UIView!
+    @IBOutlet weak var greenLampView: UIView!
+    @IBOutlet weak var switchButton: UIButton!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func switchLampsTraffic() {
+        
+        if redLampView.alpha == 1{
+            redLampView.alpha = 0.3
+            yellowLampView.alpha = 1
+        } else if yellowLampView.alpha == 1{
+            yellowLampView.alpha = 0.3
+            greenLampView.alpha = 1
+        } else if greenLampView.alpha == 1{
+            greenLampView.alpha = 0.3
+            redLampView.alpha = 1
+        } else {
+            redLampView.alpha = 1
+            switchButton.setTitle("NEXT", for: .normal)
+        }
 
+    }
+    
 }
 
